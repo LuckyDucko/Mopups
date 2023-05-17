4a06b8f027474a7e5a3c94aa17e573d297c460bc
Bryan Roth <bryan.roth@hypertherm.com>
2023-05-17 12:55:18 -0400

    #63 Fix: Issues with setup and use of configurable animations.
    Using dynamic loading of types and values.

--- SampleMaui/CSharpMarkup/MainPage.cs
+++ SampleMaui/CSharpMarkup/MainPage.cs
@@ -1,7 +1,6 @@
-﻿using AsyncAwaitBestPractices.MVVM;
-
-using Mopups.Animations;
-
+using System.Reflection;
+using AsyncAwaitBestPractices.MVVM;
+using Mopups.Animations.Base;
 using Mopups.Pages;
 using Mopups.Services;
 
@@ -14,182 +13,115 @@ namespace SampleMaui.CSharpMarkup;
 
 public partial class MainPage : ContentPage
 {
-    public double AnimationLength { get; set; }
-    public int AnimationType { get; set; }
-    public int AnimationEasing { get; set; }
+    public Picker AnimationPicker { get; set; }
+
+    public Slider AnimationLengthInSlider { get; set; }
+
+    public Picker EasingInPicker { get; set; }
 
     protected void BuildContent()
     {
         BackgroundColor = Color.FromRgb(255, 255, 255);
         Title = "Popup Demo";
         Content = new ScrollView
-        {
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            Content = GenerateMainPageStackLayout()
-        };
+                  {
+                      VerticalOptions = LayoutOptions.FillAndExpand,
+                      Content = GenerateMainPageStackLayout()
+                  };
     }
 
     private StackLayout GenerateMainPageStackLayout()
     {
         var mainStackLayout = new StackLayout
-        {
-            Spacing = 20,
+                              {
+                                  Spacing = 20,
             Margin = new Thickness(10, 15)
-        };
-        mainStackLayout.Add(PopupAnimationPicker());
-        mainStackLayout.Add(EasingAnimationPicker());
-        mainStackLayout.Add(PopupAnimationLengthSlider());
+                              };
+        mainStackLayout.Add(AnimationPicker = PopupAnimationPicker());
+        mainStackLayout.Add(EasingInPicker = EasingAnimationPicker());
+        mainStackLayout.Add(AnimationLengthInSlider = PopupAnimationLengthSlider());
         mainStackLayout.Add(GeneratePopupButton("Open Login Popup", GenerateSimpleCommandForPopup<LoginPage>()));
         mainStackLayout.Add(GeneratePopupButton("Open Aswins Popup", GenerateSimpleCommandForPopup<AswinPage>()));
         var newButton = new Button
-        {
-            Text = "New page test",
-            BackgroundColor = Color.FromArgb("#FF7DBBE6"),
-            TextColor = Color.FromRgb(255, 255, 255),
-            Command = new AsyncCommand(async () =>
-            {
-                await Navigation.PushAsync(new TestPage());
-            })
-        };
+                        {
+                            Text = "New page test",
+                            BackgroundColor = Color.FromArgb("#FF7DBBE6"),
+                            TextColor = Color.FromRgb(255, 255, 255),
+                            Command = new AsyncCommand(async () =>
+                                                       {
+                                                           await Navigation.PushAsync(new TestPage());
+                                                       })
+                        };
         mainStackLayout.Add(newButton);
 
         return mainStackLayout;
 
         Picker PopupAnimationPicker()
         {
-            var animationPicker = new Picker
-            {
-                Items = { "FadeAnimation", "MoveAnimation", "ScaleAnimation" }
-            };
-
-            animationPicker.SelectedIndexChanged += OnAnimationPickerSelectedIndexChanged;
-
-            return animationPicker;
+            return new Picker
+                   {
+                       ItemsSource = typeof(BaseAnimation)
+                                     .Assembly.GetTypes()
+                                     .Where( t => t.IsSubclassOf( typeof(BaseAnimation) ) && !t.IsAbstract )
+                                     .Select( t => (BaseAnimation)Activator.CreateInstance( t ) )
+                                     .ToList(),
+                       ItemDisplayBinding = new Binding( ".", BindingMode.OneWay, ObjectTypeNameConverter.Instance ),
+                       SelectedIndex = 0
+                   };
         }
 
         Picker EasingAnimationPicker()
         {
-            var animationPicker = new Picker
-            {
-                Items = { "Linear", "BounceIn", "BounceOut", "CubicIn", "CubicOut", "SinIn", "SinInOut", "SinOut", "SpringIn", "SpringOut" }
-            };
-
-            animationPicker.SelectedIndexChanged += OnEasingPickerSelectedIndexChanged;
-
-            return animationPicker;
-
+            return new Picker
+                   {
+                       ItemsSource = typeof(Easing).GetFields( BindingFlags.Public | BindingFlags.Static ),
+                       ItemDisplayBinding = new Binding( "Name", BindingMode.OneWay ),
+                       SelectedIndex = 0
+                   };
         }
+
         Slider PopupAnimationLengthSlider()
         {
-            var animationSlider = new Slider
-            {
-                Minimum = 0,
-                Maximum = 2000,
-                FlowDirection = FlowDirection.LeftToRight,
-
-            };
-
-            animationSlider.ValueChanged += OnSliderValueChanged;
-
-            return animationSlider;
+            return new Slider
+                   {
+                       Minimum = 0,
+                       Maximum = 2000,
+                       FlowDirection = FlowDirection.LeftToRight,
+                   };
         }
 
     }
 
-    void OnEasingPickerSelectedIndexChanged(object sender, EventArgs e)
-    {
-        var picker = (Picker)sender;
-        int selectedIndex = picker.SelectedIndex;
-        AnimationType = selectedIndex;
-    }
-
-    void OnAnimationPickerSelectedIndexChanged(object sender, EventArgs e)
-    {
-        var picker = (Picker)sender;
-        int selectedIndex = picker.SelectedIndex;
-        AnimationType = selectedIndex;
-    }
-
-    void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
-    {
-        AnimationLength = args.NewValue;
-    }
-
-    private static Button GeneratePopupButton(string buttonText, AsyncCommand buttonCommand)
+    private static Button GeneratePopupButton( string buttonText, AsyncCommand buttonCommand )
     {
         return new Button
-        {
-            Text = buttonText,
+               {
+                   Text = buttonText,
             BackgroundColor = Color.FromArgb("#FF7DBBE6"),
             TextColor = Color.FromRgb(255, 255, 255),
-            Command = buttonCommand,
-        };
+                   Command = buttonCommand,
+               };
     }
 
     private AsyncCommand GenerateSimpleCommandForPopup<TPopupPage>() where TPopupPage : PopupPage, new()
     {
         return new AsyncCommand(async () =>
-        {
-            try
             {
-                var page = new TPopupPage();
-                var easing = Easing.Linear;
-
-                switch (AnimationEasing)
+                try
                 {
-                    case 0:
-                        easing = Easing.Linear;
-                        break;
-                    case 1:
-                        easing = Easing.BounceIn;
-                        break;
-                    case 2:
-                        easing = Easing.BounceOut;
-                        break;
-                    case 3:
-                        easing = Easing.CubicIn;
-                        break;
-                    case 4:
-                        easing = Easing.CubicOut;
-                        break;
-                    case 5:
-                        easing = Easing.SinIn;
-                        break;
-                    case 6:
-                        easing = Easing.SinInOut;
-                        break;
-                    case 7:
-                        easing = Easing.SinOut;
-                        break;
-                    case 8:
-                        easing = Easing.SpringIn;
-                        break;
-                    case 9:
-                        easing = Easing.SpringOut;
-                        break;
-                }
+                    var animation = (BaseAnimation)this.AnimationPicker.SelectedItem;
 
-                switch (AnimationType)
-                {
-                    case 1:
-                        page.Animation = new FadeAnimation() { DurationIn = Convert.ToUInt32(AnimationLength), DurationOut = Convert.ToUInt32(AnimationLength), EasingIn = easing, EasingOut = easing };
-                        break;
-                    case 2:
-                        page.Animation = new MoveAnimation() { DurationIn = Convert.ToUInt32(AnimationLength), DurationOut = Convert.ToUInt32(AnimationLength), EasingIn = easing, EasingOut = easing };
-                        break;
-                    case 3:
-                        page.Animation = new ScaleAnimation() { DurationIn = Convert.ToUInt32(AnimationLength), DurationOut = Convert.ToUInt32(AnimationLength), EasingIn = easing, EasingOut = easing };
-                        break;
-                    default:
-                        break;
-                }
+                    animation.DurationIn = Convert.ToUInt32( AnimationLengthInSlider.Value );
+                    animation.DurationOut = animation.DurationIn;
+                    animation.EasingIn = (Easing)( (FieldInfo)EasingInPicker.SelectedItem ).GetValue( null );
+                    animation.EasingOut = animation.EasingIn;
 
-                await MopupService.Instance.PushAsync(page);
-            }
+                    await MopupService.Instance.PushAsync( new TPopupPage { Animation = animation } );
+                }
             catch (Exception)
-            {
-                throw;
-            }
+                {
+                    throw;
+                }
         });
     }
 }
---
+++ SampleMaui/CSharpMarkup/ObjectTypeNameConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace SampleMaui.CSharpMarkup;
+
+public sealed class ObjectTypeNameConverter : IValueConverter
+{
+    public static readonly IValueConverter Instance = new ObjectTypeNameConverter();
+
+#region Implementation of IValueConverter
+
+    /// <inheritdoc />
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // Invert the bool
+        return value.GetType().Name;
+    }
+
+    /// <inheritdoc />
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        //return value.ToString();
+        throw new NotSupportedException($"{nameof(ObjectTypeNameConverter)}.{nameof(this.ConvertBack)}");
+    }
+
+#endregion
+}
